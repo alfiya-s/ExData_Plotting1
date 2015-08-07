13c156da7858b761b070103af3e25f0b62ef3674
Alfiya Sagitova <fifiusha@gmail.com>
2015-08-07 22:38:44 +0300

    Final draft

--- plot1.R
+++ plot1.R
@@ -1,12 +1,9 @@
 arch <- unz('exdata-data-household_power_consumption.zip', 
             'household_power_consumption.txt')
 househ.data <- read.table(arch, sep = ";", header = TRUE, na.strings = "?")
-close(arch)
-head(househ.data$Date)
-househ.data$Date <- as.Date(househ.data$Date, "%d/%m/%Y")
-househ.subset <- househ.data[househ.data$Date <= as.Date("2007-02-02") & 
-                             househ.data$Date >= as.Date("2007-02-01"), ]
+png("Rplot1.png", 480, 480)
 hist(househ.subset$Global_active_power, 
      xlab = "Global Active Power (kilowatts)", 
      col = "red",
      main = "Global Active Power")
+dev.off()
--- plot4.R
+++ plot4.R
@@ -4,11 +4,17 @@ househ.data <- read.table(arch, sep = ";", header = TRUE, na.strings = "?")
 househ.data$Date <- as.Date(househ.data$Date, "%d/%m/%Y")
 househ.subset <- househ.data[househ.data$Date <= as.Date("2007-02-02") & 
                                      househ.data$Date >= as.Date("2007-02-01"), ]
-
-
 househ.subset$dttime <- strptime(paste(househ.subset$Date, 
                                        househ.subset$Time), "%Y-%m-%d %H:%M:%S") 
-png("Rplot3.png", 480, 480)
+
+png(filename = "Rplot4.png", width = 480, height = 480)
+par(mfcol = c(2,2))
+par(mar = c(4,4,2,2))
+plot(househ.subset$dttime, 
+     househ.subset$Global_active_power, 
+     type = "l",
+     ylab = "Global Active Power",
+     xlab = "") 
 plot(househ.subset$dttime, 
      househ.subset$Sub_metering_1,
      type = "l",
@@ -19,5 +25,15 @@ plot(househ.subset$dttime,
                 househ.subset$Sub_metering_3)) 
 lines(househ.subset$dttime, househ.subset$Sub_metering_2, col = 2, type = "l")
 lines(househ.subset$dttime, househ.subset$Sub_metering_3, col = 4, type = "l")
-legend("topright", lty =1, names(househ.data)[7:9], col = c(1, 2, 4))
+legend("topright", lty =1, names(househ.data)[7:9], col = c(1, 2, 4), bty = "n")
+plot(househ.subset$dttime, 
+     househ.subset$Voltage, 
+     type = "l",
+     ylab = "Voltage",
+     xlab = "datetime") 
+plot(househ.subset$dttime, 
+     househ.subset$Global_reactive_power, 
+     type = "l",
+     ylab = "Global_reactive_power",
+     xlab = "datetime") 
 dev.off()
